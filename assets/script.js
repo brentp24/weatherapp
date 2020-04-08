7063f9cc7def91409da3291b7c0bc3325cda57cf
Brent Peterson <brentp24@gmail.com>
2020-04-08 11:30:44 -0600

    search function

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,108 @@
-// call from weather API
 
-let cityArray = [];
-let todaysDate = moment().format("l");  
 
-var day1 = moment(todaysDate).add(1,'day').format("l");  
-var day2 = moment(todaysDate).add(2,'day').format("l");  
-var day3 = moment(todaysDate).add(3,'day').format("l");  
-var day4 = moment(todaysDate).add(4,'day').format("l");  
-var day5 = moment(todaysDate).add(5,'day').format("l");  
+
+var cityTarget;
+var cityArray = [];
+let todaysDate = moment().format("l");
+var day1 = moment(todaysDate).add(1, 'day').format("l");
+var day2 = moment(todaysDate).add(2, 'day').format("l");
+var day3 = moment(todaysDate).add(3, 'day').format("l");
+var day4 = moment(todaysDate).add(4, 'day').format("l");
+var day5 = moment(todaysDate).add(5, 'day').format("l");
 var forecast = [day1, day2, day3, day4, day5];
+$(".todays-date").text(todaysDate);
+// Need to get my own API Key
+var apikey = "166a433c57516f51dfab1f7edaed8413";
 
-// This is our API key
-let apikey = "166a433c57516f51dfab1f7edaed8413";
 
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=Bujumbura,Burundi&appid=" + apikey;
 
- var query5day = "https://api.openweathermap.org/data/2.5/forecast/daily?" + "q=Bujumbura,Burundi&appid=" + apikey; 
+// click search button
+$("#search-icon").on("click", function () {
+    //assigns user input to city
+    cityTarget = $("#search").val().trim();
+    $("#city-name").val(cityTarget);
+    //store in local storage
+    cityArray.push(cityTarget);
+    localStorage.setItem("cityTarget", cityTarget);
+    localStorage.setItem("cityArray",JSON.stringify(cityArray));
+    var stored = JSON.parse(localStorage.getItem("cityArray"));
+    console.log(stored);
+    //if no text in input return nothing
+    if ($("#search").val() === "") {
+        return;
+    }
+    //display city info
+    displayTodaysWeather();
+    display5day();
 
-// Here we run our AJAX call to the OpenWeatherMap API
+    //add city to city list
+    $(".city-list").empty();
+    var reversedStored = stored.reverse();
+    for (i=0;i < Math.min(8,reversedStored.length); i++) {
+    var cityList = $(".city-list");
+    var newListItem = $("<div/>").attr({
+        class: "btn btn-light",
+        type: "button",
+        id: "btn_" + reversedStored[i],
+        value: reversedStored[i],
+    });
+   
+    
+    cityList.append(newListItem);
+    newListItem.append(reversedStored[i]);
+     
+    }
+    // this drove me crazy.... I couldn't figure out why the selection wouldn't persist. 
+    $("#search").val("");
+    event.preventDefault();
+    
+    
+});
+
+// click list item button
+$("body").on("click", ".btn", function () {
+    cityTarget = $(this).html();
+    console.log($(this).html());
+    displayTodaysWeather();
+    display5day();
+    $("#search").val("");
+    event.preventDefault();
+})
+
+
+// Today's Weather
 function displayTodaysWeather() {
+    
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityTarget + "&appid=" + apikey;
+
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         // store object data
         .then(function (response) {
-
             // Transfer content to HTML
             $(".city").html(response.name);
             var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-            $(".tempF").text(tempF.toFixed(2));
+            $(".tempF").text(tempF.toFixed(0));
             $(".humidity").text(response.main.humidity);
             $(".wind").text(response.wind.speed);
             $(".weather").text(response.weather[0].main);
+        }).catch(function (error) {
+            console.log(error);
         });
-        $(".todays-date").text(todaysDate);
 }
 
+
 var forecast5Day = $(".forecast");
+
 function display5day() {
+    
+// build query 
+    var query5day = "https://api.openweathermap.org/data/2.5/forecast/daily?" + "q=" + cityTarget + "&appid=" + apikey;
+    //clear old results
+    $("#forecast-row").empty();
     $.ajax({
         url: query5day,
         method: "GET"
@@ -49,49 +111,49 @@ function display5day() {
         .then(function (response) {
 
             //create new card
-            var i; 
-        for (i=0; i < forecast.length; i++) {  
-            var newDay = $("<div/>").attr({
-                class: "card",
-            });
-            var newCard = $("<div/>").attr({
-                class: "card-body",
-            });
-            var newDate = $("<div/>").attr({
-                class: "card-title",
-                id: forecast[i],
-            });
-            var newWeather = $("<div/>").attr({
-                class: "weather-day",
-                id: forecast[i],
-            });
-            var newTemp = $("<div/>").attr({
-                class: "tempF-day",
-                id: forecast[i],
-            });
-            var newHumidity = $("<div/>").attr({
-                class: "humidity-day",
-                id: forecast[i],
-            });
-        //append cards
+            var i;
+            for (i = 0; i < forecast.length; i++) {
+                var newDay = $("<div/>").attr({
+                    class: "card",
+                });
+                var newCard = $("<div/>").attr({
+                    class: "card-body",
+                });
+                var newDate = $("<div/>").attr({
+                    class: "card-title",
+                    id: forecast[i],
+                });
+                var newWeather = $("<div/>").attr({
+                    class: "weather-day",
+                    id: forecast[i],
+                });
+                var newTemp = $("<div/>").attr({
+                    class: "tempF-day",
+                    id: forecast[i],
+                });
+                var newHumidity = $("<div/>").attr({
+                    class: "humidity-day",
+                    id: forecast[i],
+                });
+                //append cards
                 // append elements
                 forecast5Day.append(newDay);
                 newDay.append(newCard);
                 newCard.append(newDate, newWeather, newTemp, newHumidity);
-    
 
-            // pull per data per day
-            newDate.text(forecast[i]);
-            newTemp.text("Temp (F): "+((response.list[i].temp.day - 273.15) * 1.80 + 32).toFixed(2));
-            newHumidity.text("Humidity: " + response.list[i].humidity + "%");
-            newWeather.text(response.list[i].weather[0].main);
 
-        }
+                // pull per data per day
+                newDate.text(forecast[i]);
+                newTemp.text("Temp: " + ((response.list[i].temp.day - 273.15) * 1.80 + 32).toFixed(0) + " °F");
+                newHumidity.text("Humidity: " + response.list[i].humidity + "%");
+                newWeather.text(response.list[i].weather[0].main);
 
+            }
+        }).catch(function (error) {
+            console.log(error);
         });
 
 }
 
-
-displayTodaysWeather();
-display5day();
+// displayTodaysWeather();
+// display5day(); 
